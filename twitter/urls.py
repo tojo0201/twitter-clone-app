27d13c3709e99683b8b17925d0e0aab797a244e0
tojo0201 <east.castle.lotus@gmail.com>
2021-11-16 18:55:19 +0900

    URLをハイフンに修正

--- twitter/urls.py
+++ twitter/urls.py
@@ -6,8 +6,8 @@ app_name = 'twitter'
 urlpatterns = [
     path('', views.top, name='top'),
     path('tweet-input/', views.TweetInputView.as_view(), name='tweet_input'),
-    path('user_list/', views.UserListView.as_view(), name='user_list'),
-    path('other_user/<int:other_user_id>/', views.other_user, name='other_user'),
+    path('user-list/', views.UserListView.as_view(), name='user_list'),
+    path('other-user/<int:other_user_id>/', views.other_user, name='other_user'),
     path('followed/<int:other_user_id>/', views.user_followed, name='followed'),
-    path('follow_deleted/<int:other_user_id>/', views.follow_deleted, name='follow_deleted'),
+    path('follow-deleted/<int:other_user_id>/', views.follow_deleted, name='follow_deleted'),
 ]
